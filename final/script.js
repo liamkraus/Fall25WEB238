df0309f20b42bfaa8b25d9bf6b550be2e074e379
liamkraus <liamkraus3@gmail.com>
2025-12-06 11:06:36 -0600

    Create script.js

---
+++ final/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+  // 1 User interaction + content change
+  $('#changeTextBtn').click(function () {
+    $('#introText').text('This text was updated using jQuery!');
+  });
+
+  // 2️ User-initiated animation (slide toggle)
+  $('#toggleCardsBtn').click(function () {
+    $('.card').slideToggle();
+  });
+
+  // 3 User interaction (hover effect)
+  $('.card').hover(
+    function () {
+      $(this).addClass('highlight');
+    },
+    function () {
+      $(this).removeClass('highlight');
+    }
+  );
+
+  // 4️ User-initiated animation (fade effect)
+  $('#fadeBtn').click(function () {
+    $('#fadeMessage').fadeToggle();
+  });
+
+  // 5️ Animation (smooth scrolling navigation)
+  $('nav a, footer a').click(function (e) {
+    e.preventDefault();
+    const target = $(this).attr('href');
+    $('html, body').animate(
+      { scrollTop: $(target).offset().top },
+      600
+    );
+  });
+
+});
